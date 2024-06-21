a46d8057d450a48eefd23d244196483c37eddb10
Lekta23 <48383181+Lekta23@users.noreply.github.com>
2024-06-21 18:13:35 +0200

    Reacto Cypress OK

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,128 +1,107 @@
 describe('Tusmo Game', () => {
-  it('Visit', () => {
-    cy.visit('http://localhost:3000')
-  })
+  const baseUrl = 'http://localhost:3000';
+  const apiEndpoint = 'https://trouve-mot.fr/api/random/1';
+  const wordsFixture = { fixture: 'words.json' };
 
-  it('check word is init',  () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('#input_1').should('have.text', '_')
-    cy.get('#input_2').should('have.text', '_')
-    cy.get('#input_3').should('have.text', '_')
-  })
+  beforeEach(() => {
+    cy.visit(baseUrl);
+    cy.intercept('GET', apiEndpoint, wordsFixture);
+    cy.wait(1000);
+  });
 
-  it('keypress a letter set letter in span',  () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('u')
-    cy.get('#input_0').should('have.text', 'U')
-  })
+  it('should initialize the word', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('#input_1').should('have.text', '_');
+    cy.get('#input_2').should('have.text', '_');
+    cy.get('#input_3').should('have.text', '_');
+  });
 
-  it('keypress a word set letter in span',  () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('uais')
-    cy.get('#input_0').should('have.text', 'U')
-    cy.get('#input_1').should('have.text', 'A')
-    cy.get('#input_2').should('have.text', 'I')
-    cy.get('#input_3').should('have.text', 'S')
-  })
+  it('should set letter in span on keypress', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('u');
+    cy.get('#input_0').should('have.text', 'U');
+  });
 
-  it('keypress delete key delete letter in span',  () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('u')
-    cy.get('#input_0').should('have.text', 'U')
-    cy.get('body').type('{backspace}')
-    cy.get('#input_0').should('have.text', '_')
-  })
+  it('should set multiple letters in span on keypress', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('uais');
+    cy.get('#input_0').should('have.text', 'U');
+    cy.get('#input_1').should('have.text', 'A');
+    cy.get('#input_2').should('have.text', 'I');
+    cy.get('#input_3').should('have.text', 'S');
+  });
 
-  it('if word have 4 letter and user type 6 letter just show the first 4 letter',  () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-        cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('usmoouaisEtoile')
-    cy.get('#input_0').should('have.text', 'U')
-    cy.get('#input_1').should('have.text', 'S')
-    cy.get('#input_2').should('have.text', 'M')
-    cy.get('#input_3').should('have.text', 'O')
-  })
-  
-  it('click on validate button and show the word',   () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('usmo')
-    cy.get('#validate_button').click()
-    cy.get('#result_word_0 > #result_letter_0').should('have.class', 'bg-red-500')
-    cy.get('#result_word_0 >#result_letter_1').should('have.class', 'bg-red-500')
-    cy.get('#result_word_0 >#result_letter_2').should('have.class', 'bg-yellow-500')
-    cy.get('#result_word_0 >#result_letter_3').should('have.class', 'bg-blue-500')
-    cy.get('#result_word_0 > #result_letter_4').should('have.class', 'bg-yellow-500')
-  })
+  it('should delete letter in span on backspace keypress', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('u');
+    cy.get('#input_0').should('have.text', 'U');
+    cy.get('body').type('{backspace}');
+    cy.get('#input_0').should('have.text', '_');
+  });
 
-  it('click on validate button and show the word but with history',   () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('usmo')
-    cy.get('#validate_button').click()
-    cy.get('body').type('hles')
-    cy.get('#validate_button').click()
-    cy.get('#result_word_0 > #result_letter_0').should('have.class', 'bg-red-500')
-    cy.get('#result_word_0 > #result_letter_1').should('have.class', 'bg-red-500')
-    cy.get('#result_word_0 > #result_letter_2').should('have.class', 'bg-yellow-500')
-    cy.get('#result_word_0 > #result_letter_3').should('have.class', 'bg-blue-500')
-    cy.get('#result_word_0 > #result_letter_4').should('have.class', 'bg-yellow-500')
-    cy.get('#result_word_1 > #result_letter_0').should('have.class', 'bg-red-500')
-    cy.get('#result_word_1 > #result_letter_1').should('have.class', 'bg-blue-500')
-    cy.get('#result_word_1 > #result_letter_2').should('have.class', 'bg-blue-500')
-    cy.get('#result_word_1 > #result_letter_3').should('have.class', 'bg-blue-500')
-    cy.get('#result_word_1 > #result_letter_4').should('have.class', 'bg-red-500')
-  })
+  it('should show only the first 4 letters if more than 4 are typed', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('usmoouaisEtoile');
+    cy.get('#input_0').should('have.text', 'U');
+    cy.get('#input_1').should('have.text', 'S');
+    cy.get('#input_2').should('have.text', 'M');
+    cy.get('#input_3').should('have.text', 'O');
+  });
 
-  it('send message when the word is found',   () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('usmo')
-    cy.get('#validate_button').click()
-    cy.get('body').type('hles')
-    cy.get('#validate_button').click()
-    cy.get('body').type('uais')
-    cy.get('#validate_button').click()
-    cy.get('#error_label').should('have.text', 'Vous avez trouvé le mot !')
-  })
+  it('should show the word on validate button click', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('usmo');
+    cy.get('#validate_button').click();
+    cy.get('#result_word_0 > #result_letter_0').should('have.class', 'bg-red-500');
+    cy.get('#result_word_0 > #result_letter_1').should('have.class', 'bg-red-500');
+    cy.get('#result_word_0 > #result_letter_2').should('have.class', 'bg-yellow-500');
+    cy.get('#result_word_0 > #result_letter_3').should('have.class', 'bg-blue-500');
+    cy.get('#result_word_0 > #result_letter_4').should('have.class', 'bg-yellow-500');
+  });
 
-  it('show message when the word is not found', () => {
-    cy.visit('http://localhost:3000')
-    cy.intercept('GET', 'https://trouve-mot.fr/api/random/1', { fixture: 'words.json' })
-    cy.wait(1000)
-    cy.get('#input_0').should('have.text', '_')
-    cy.get('body').type('usmo')
-    cy.get('#validate_button').click()
-    cy.get('body').type('hles')
-    cy.get('#validate_button').click()
-    cy.get('body').type('hfls')
-    cy.get('#validate_button').click()
-    cy.get('body').type('popo')
-    cy.get('#validate_button').click()
-    cy.get('body').type('ytrp')
-    cy.get('#validate_button').click()
-    cy.get('body').type('jfdl')
-    cy.get('#validate_button').click()
-    cy.get('#error_label').should('have.text', 'Vous avez épuisé vos essais. Le mot était OUAIS.')
-  })
-})
+  it('should show the word with history on multiple validate button clicks', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('usmo');
+    cy.get('#validate_button').click();
+    cy.get('body').type('hles');
+    cy.get('#validate_button').click();
+    cy.get('#result_word_0 > #result_letter_0').should('have.class', 'bg-red-500');
+    cy.get('#result_word_0 > #result_letter_1').should('have.class', 'bg-red-500');
+    cy.get('#result_word_0 > #result_letter_2').should('have.class', 'bg-yellow-500');
+    cy.get('#result_word_0 > #result_letter_3').should('have.class', 'bg-blue-500');
+    cy.get('#result_word_0 > #result_letter_4').should('have.class', 'bg-yellow-500');
+    cy.get('#result_word_1 > #result_letter_0').should('have.class', 'bg-red-500');
+    cy.get('#result_word_1 > #result_letter_1').should('have.class', 'bg-blue-500');
+    cy.get('#result_word_1 > #result_letter_2').should('have.class', 'bg-blue-500');
+    cy.get('#result_word_1 > #result_letter_3').should('have.class', 'bg-blue-500');
+    cy.get('#result_word_1 > #result_letter_4').should('have.class', 'bg-red-500');
+  });
+
+  it('should show a success message when the word is found', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('usmo');
+    cy.get('#validate_button').click();
+    cy.get('body').type('hles');
+    cy.get('#validate_button').click();
+    cy.get('body').type('uais');
+    cy.get('#validate_button').click();
+    cy.get('#error_label').should('have.text', 'Vous avez trouvé le mot !');
+  });
+
+  it('should show an error message when the word is not found', () => {
+    cy.get('#input_0').should('have.text', '_');
+    cy.get('body').type('usmo');
+    cy.get('#validate_button').click();
+    cy.get('body').type('hles');
+    cy.get('#validate_button').click();
+    cy.get('body').type('hfls');
+    cy.get('#validate_button').click();
+    cy.get('body').type('popo');
+    cy.get('#validate_button').click();
+    cy.get('body').type('ytrp');
+    cy.get('#validate_button').click();
+    cy.get('body').type('jfdl');
+    cy.get('#validate_button').click();
+    cy.get('#error_label').should('have.text', 'Vous avez épuisé vos essais. Le mot était OUAIS.');
+  });
+});
