37cde6795f90d869fb72376e51194167171b48cc
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-06-21 15:04:28 +0200

    feat: 🎸 black bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black p-4">
       <h1 className="text-4xl font-bold mb-4">Tusmo</h1>
       <input
         type="text"
